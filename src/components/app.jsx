7094c4059afaec81e46aa5186b774031bbcb5594
thibaud marce <thibaud@captaincontrat.com>
2018-07-13 15:26:48 +0200

    Add google map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import FlatList from './flatList.jsx';
+import GoogleMap from './googleMap.jsx'
 import flats from '../../data/flats.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            flats: flats,
-        }
+    this.state = {
+      flats: flats,
     }
+  }
 
-    render() {
-        return (
-            <div>
-                <div className='flat-list'>
-                    <FlatList flats={this.state.flats} />
-                </div>
-                <div className='map-container'>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className='flat-list'>
+          <FlatList flats={this.state.flats} />
+        </div>
+        <div className='map-container'>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
